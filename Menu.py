816c64d0311da61fa3bf44dbd2a7579f22bec8bb
IzLeandro <34462612+IzLeandro@users.noreply.github.com>
2020-10-14 03:07:54 -0600

    Validaciones y solicitud de datos

--- Menu.py
+++ Menu.py
@@ -2,4 +2,31 @@
 # Fecha de Creación: 13/10/2020 10:00pm
 # Fecha de última Modificación: #!
 # Versión: 3.8
+import re
+
+def solicitarYValidarDatos():
+    nombreCompleto=str(input("Digite su nombre completo: "))
+
+    fechaDeNacimiento=str(input("Digite su fecha de nacimiento(Sea cuidadoso de usar el formado AAAA/MM/DD): "))
+    if bool(re.match("\d{4}(?P<sep>[-/])\d{2}(?P=sep)\d{2}",fechaDeNacimiento)) == False:     
+        input("Compruebe que esté siguiendo correctamente el formato solicitado al ingresar la fecha.\nPresione enter para continuar...")
+        return ""
+
+    altura=input("Digite su altura: (ej: 1.80)")
+    try:
+        altura=float(altura)
+    except:
+        input("Compruebe que esté siguiendo correctamente el formato solicitado al ingresar la altura.\nPresione enter para continuar...")
+        return ""
+    sexo=str(input("Indique su sexo(M o F): "))
+    if sexo.upper() =="M":
+        sexo=True
+    elif sexo.upper()=="F":
+        sexo=False
+    else:
+        input("Ingrese un valor correcto en el apartado del sexo.\nPresione enter para continuar...")
+        return ""
+    return [nombreCompleto,fechaDeNacimiento,altura,sexo]
+
+solicitarYValidarDatos()
 

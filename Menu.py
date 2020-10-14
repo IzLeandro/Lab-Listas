8978314e7add60c3f11a3b01ad736ef584e413a8
IzLeandro <34462612+IzLeandro@users.noreply.github.com>
2020-10-14 15:05:54 -0600

    waiting for intro added

--- Menu.py
+++ Menu.py
@@ -48,5 +48,6 @@ def mainMenu():
     if paciente=="":
         return ""
     print(paciente[0],", usted nació un día",diaParImpar(paciente[1]),"del mes de",nombreMes(paciente[1]),", usted tiene",getYears(paciente[1]),"años, que visto en binario es",yearToBin(paciente[1]), "\nSu signo sodiacal es:",getHoroscope(paciente[1]),"por ende su personalidad es",personalidad(paciente[1]),", su sexo es",["femenino","masculino"][paciente[3]],"y mide",paciente[2],"cm.")
+    input("\nPresione enter para continuar...")
     return
 mainMenu()
